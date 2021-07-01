434e0628ad69bd2eee8267414d74f547c1048161
NaruseII <naruse.dev@gmail.com>
2021-07-01 13:55:33 +0200

    version

--- src/fr/naruse/servermanager/core/ServerManager.java
+++ src/fr/naruse/servermanager/core/ServerManager.java
@@ -15,7 +15,7 @@ import java.util.Set;
 
 public class ServerManager {
 
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     private static ServerManager instance;
     public static ServerManager get() {
